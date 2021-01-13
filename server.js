acd59546b09496ac2c086cabc350703dd356392b
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-13 13:34:30 +0400

    add mongodb connection

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => res.send("API running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on Port: ${PORT}`));
