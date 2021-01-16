960972813699be509f7a191a6f277807320e3d0f
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-17 00:00:17 +0400

    add get profiless and profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -120,4 +120,36 @@ router.post(
   }
 );
 
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not Found" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not Found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
