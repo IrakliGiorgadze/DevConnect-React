8cd51cddf05ebde6f541f3fa0ae34c72e8ec906d
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-17 01:30:58 +0400

    delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -208,4 +208,23 @@ router.put(
   }
 );
 
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
