ac6b986d2376a2e2b77ea3fc71c2dcc9d3d5e96f
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-18 00:59:13 +0400

    add unlike to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -111,4 +111,30 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not been liked" });
+    }
+
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
