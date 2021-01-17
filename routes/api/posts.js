0edd73877ff5db96e043aacd18ec6e5f4d7d7038
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-18 00:45:27 +0400

    add likes to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -89,4 +89,26 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post is already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
