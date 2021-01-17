8436bf1052f04a3fd15f5b2fb429ada68ae6deb3
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-17 23:44:25 +0400

    get posts and get posts by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,33 @@ router.post(
   }
 );
 
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
