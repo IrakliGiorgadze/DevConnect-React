0021162c756f1463a50c0bcd27ff540ac58708b5
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-18 17:46:47 +0400

    add register and sign in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -16,12 +17,12 @@ const Register = () => {
       [e.target.name]: e.target.value,
     });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not macth");
     } else {
-      console.log(formData);
+      console.log("Success");
     }
   };
 
@@ -51,10 +52,6 @@ const Register = () => {
             onChange={(e) => onChange(e)}
             required
           />
-          <small className="form-text">
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
-          </small>
         </div>
         <div className="form-group">
           <input
@@ -79,7 +76,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
