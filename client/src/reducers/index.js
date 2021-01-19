1d8d36cf209cae90283d5d458d9f1a6e7108f474
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-19 15:24:30 +0400

    implement errors in alert

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux";
-import alert from "./alert";
 
-export default combineReducers({ alert });
+import alert from "./alert";
+import auth from "./auth";
+
+export default combineReducers({ alert, auth });
