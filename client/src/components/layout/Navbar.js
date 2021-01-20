df66f56af067aeb335a0180c0776ef146dbb7a14
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-20 20:35:38 +0400

    add dashboard styling, spinner and protect route

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,10 +8,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <a onClick={logout} to="#!">
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i> <span className="hide-sm"></span>
+          Dashboard
+        </Link>
+      </li>
+      <li>
+        <Link onClick={logout} to="#!">
           <i className="fas fa-sign-out-alt"></i>{" "}
           <span className="hide-sm">Logout</span>
-        </a>
+        </Link>
       </li>
     </ul>
   );
