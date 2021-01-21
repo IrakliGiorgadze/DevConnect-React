38e5860f6541ab4688142e871ec10032360f1263
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-21 17:27:30 +0400

    add experience form and actions

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import { Provider } from "react-redux";
@@ -49,6 +50,11 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </Switch>
           </section>
