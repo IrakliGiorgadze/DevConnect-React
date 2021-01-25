a84e3db85a356a3d43fc1c65b156e6cfd6c2910f
IrakliGiorgadze <ika.giorgadze@gmail.com>
2021-01-25 23:19:58 +0400

    implement likes and unlikes

---
+++ client/src/actions/post.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/posts");
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
+
+const PostItem = ({
+  addLike,
+  removeLike,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+}) => {
+  return (
+    <div class="post bg-white p-1 my-1">
+      <div>
+        <a href="profile.html">
+          <img class="round-img" src={avatar} alt="" />
+          <h4>{name}</h4>
+        </a>
+      </div>
+      <div>
+        <p class="my-1">{text}</p>
+        <p class="post-date">
+          Posted on <Moment format="YYY/MM/DD">{date}</Moment>
+        </p>
+        <button
+          onClick={(e) => addLike(_id)}
+          type="button"
+          class="btn btn-light"
+        >
+          <i class="fas fa-thumbs-up"></i>{" "}
+          <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+        </button>
+        <button
+          onClick={(e) => removeLike(_id)}
+          type="button"
+          class="btn btn-light"
+        >
+          <i class="fas fa-thumbs-down"></i>
+        </button>
+        <Link href={`/post/${_id}`} class="btn btn-primary">
+          Discussion{" "}
+          {comments.length > 0 && (
+            <span class="comment-count">{comments.length}</span>
+          )}
+        </Link>
+        {!auth.loading && user === auth.user._id && (
+          <button type="button" class="btn btn-danger">
+            <i class="fas fa-times"></i>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToprops = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToprops, { addLike, removeLike })(PostItem);
